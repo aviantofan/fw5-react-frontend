7e9394d5abe719efe75fa4cb5647f94a55004264
eexxprd <aviantofan@gmail.com>
2022-02-28 13:56:50 +0700

    feat: add button plus minus

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,32 +1,38 @@
-// import React from "react"
+import React from "react"
+import { FaPlus, FaMinus } from 'react-icons/fa'
 
-// export class Button extends React.Component {
-//   state = {
-//     angka: 0
-//   }
-//   plus = () => {
-//     this.setState({
-//       angka: this.state.angka + 1
-//     })
-//   }
-//   minus = () => {
-//     this.setState({
-//       angka: this.state.angka - 1
-//     })
-//   }
+export class Button extends React.Component {
+  state = {
+    angka: 0
+  }
+  plus = () => {
+    this.setState({
+      angka: this.state.angka + 1
+    })
+  }
+  minus = () => {
+    if (this.state.angka > 0) {
+      this.setState({
+        angka: this.state.angka - 1
+      })
+    }
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         {this.props.sapa}
-//         <div>
-//           <button onClick={this.plus}>+</button>
-//           {this.state.angka}
-//           <button onClick={this.minus}>-</button>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div className="day d-flex row justify-content-between align-items-center">
+        <div className="col">
+          <button onClick={this.plus} className="plus"><FaPlus /></button>
+        </div>
+        <div className="col">
+          <div className="count">{this.state.angka}</div>
+        </div>
+        <div className="col">
+          <button onClick={this.minus} className="minus"><FaMinus /></button>
+        </div>
+      </div>
+    )
+  }
+}
 
-// export default Button
+export default Button
