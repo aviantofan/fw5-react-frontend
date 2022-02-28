4cb2bd04c44d61b883cf7111beac043df6c1713e
eexxprd <aviantofan@gmail.com>
2022-02-28 13:57:16 +0700

    chore: add reversebutton plus minus

---
+++ src/components/Reversebutton.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+export class Reversebutton extends React.Component {
+  state = {
+    angka: 0
+  }
+  plus = () => {
+    this.setState({
+      angka: this.state.angka + 1
+    })
+  }
+  minus = () => {
+    if (this.state.angka > 0) {
+      this.setState({
+        angka: this.state.angka - 1
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="day d-flex row justify-content-between align-items-center">
+        <div className="col">
+          <button onClick={this.minus} className="minus"><FaMinus /></button>
+        </div>
+        <div className="col">
+          <div className="count">{this.state.angka}</div>
+        </div>
+        <div className="col">
+          <button onClick={this.plus} className="plus"><FaPlus /></button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Reversebutton
