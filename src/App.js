5bce931008aea7ed72af718991ca729447f5dafa
eexxprd <aviantofan@gmail.com>
2022-02-28 13:55:27 +0700

    feat: fix routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-// import './App.css';
-// import Button from './components/Button';
-// import logo from './logo.svg';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           <Button sapa="Selamat Siang" />
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default  App
 
 import React, { Component } from 'react'
 import Homepage from './pages/Homepage'
+// import Homeloggedpage from './pages/Homeloggedpage'
 import Vehicletypepage from './pages/Vehicletypepage'
 import Registerpage from './pages/Registerpage'
 import Loginpage from './pages/Loginpage'
@@ -40,29 +16,40 @@ import Vehiclelistpage from './pages/Vehiclelistpage'
 import { unstable_HistoryRouter as HistoryRouter, /*BrowserRouter*/ Route, Routes } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 export default class App extends Component {
-  componentDidMount() {
-    console.log(this.props)
-  }
+  // state = {
+  //   isLogged: false,
+  //   greetings: ''
+  // }
+  // toggle = () => {
+  //   this.setState({ isLogged: !this.state.isLogged })
+  // }
+  // componentDidMount() {
+  //   console.log(this.props)
+  // }
   history = createBrowserHistory()
   render() {
     return (
-      <HistoryRouter history={this.history}>
-        {/* <BrowserRouter> */}
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='vehicleType' element={<Vehicletypepage />} />
-          <Route path='vehicles' element={<Vehiclelistpage />} />
-          <Route path='vehicles/:id' element={<Vehicledetailpage />} />
-          <Route path='login' element={<Loginpage />} />
-          <Route path='register' element={<Registerpage />} />
-          <Route path='forgotPassword' element={<Forgotpasswordpage />} />
-          <Route path='payment' element={<Paymentpage />} />
-          <Route path='profile' element={<Profilepage />} />
-          <Route path='history' element={<Historypage />} />
-          <Route path='reservation' element={<Reservationpage />} />
-        </Routes>
-        {/* </BrowserRouter> */}
-      </HistoryRouter>
+      <div>
+        <HistoryRouter history={this.history}>
+          {/* {!this.state.isLogged && <Homepage onLogin={(value) => { this.setState({ isLogged: value }) }} />}
+          {this.state.isLogged && <Homeloggedpage />} */}
+          {/* <BrowserRouter> */}
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='vehicleType' element={<Vehicletypepage />} />
+            <Route path='vehicles' element={<Vehiclelistpage />} />
+            <Route path='vehicles/:id' element={<Vehicledetailpage />} />
+            <Route path='login' element={<Loginpage />} />
+            <Route path='register' element={<Registerpage />} />
+            <Route path='forgotPassword' element={<Forgotpasswordpage />} />
+            <Route path='payment/:id' element={<Paymentpage />} />
+            <Route path='profile' element={<Profilepage />} />
+            <Route path='history' element={<Historypage />} />
+            <Route path='reservation/:id' element={<Reservationpage />} />
+          </Routes>
+          {/* </BrowserRouter> */}
+        </HistoryRouter>
+      </div>
     )
   }
 }
