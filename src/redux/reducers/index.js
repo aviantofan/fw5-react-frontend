519222eafc641be5bd4be85e2b5c28d7dbccac3b
eexxprd <aviantofan@gmail.com>
2022-03-06 02:37:07 +0700

    feat: add categoryMotorbike and categoryBike

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from "redux";
 import auth from './auth'
 import vehiclePopular from './vehiclePopular'
 import vehicleCategoryCar from './vehicleCategoryCar'
+import vehicleCategoryMotorbike from "./vehicleCategoryMotorbike";
+import vehicleCategoryBike from "./vehicleCategoryBike";
 
 const rootReducers = combineReducers({
   auth,
   vehiclePopular,
-  vehicleCategoryCar
+  vehicleCategoryCar,
+  vehicleCategoryMotorbike,
+  vehicleCategoryBike
 })
 
 export default rootReducers
