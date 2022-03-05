4bf798d28fb438426582796316280bf84cdbf966
eexxprd <aviantofan@gmail.com>
2022-03-06 02:35:47 +0700

    chore: add vehicle categoryMotorbike

---
+++ src/redux/actions/vehicleCategoryMotorbike.js
@@ -0,0 +1,8 @@
+import http from "../../helpers/http"
+
+export const getCategoryMotorbike = () => {
+  return {
+    type: 'GET_MOTORBIKE',
+    payload: http().get('/vehicles/category?categoryId=2&limit=4')
+  }
+}
---
+++ src/redux/reducers/vehicleCategoryMotorbike.js
@@ -0,0 +1,37 @@
+const dataState = {
+  vehicleCategoryMotorbike: [],
+  pageInfo: {},
+  isLoading: false,
+  error: false,
+  errorMsg: ''
+}
+
+const vehicleCategoryMotorbike = (state = dataState, action) => {
+  switch (action.type) {
+    case 'GET_MOTORBIKE_PENDING': {
+      state.isLoading = true
+      state.isError = false
+      return { ...state }
+    }
+    case 'GET_MOTORBIKE_FULFILLED': {
+      const { data } = action.payload
+      state.vehicleCategoryMotorbike = data.results
+      state.pageInfo = data.pageInfo
+      state.isLoading = false
+      state.isError = false
+      return { ...state }
+    }
+    case 'GET_MOTORBIKE_REJECTED': {
+      const { message } = action.payload.response.data
+      state.isLoading = false
+      state.isError = true
+      state.errorMsg = message
+      return { ...state }
+    }
+    default: {
+      return { ...state }
+    }
+  }
+}
+
+export default vehicleCategoryMotorbike
