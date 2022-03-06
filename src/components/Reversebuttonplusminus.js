64185c6e75df9c0936fcac8a7a03c43595998fec
eexxprd <aviantofan@gmail.com>
2022-03-06 20:05:02 +0700

    feat: add counter with redux

--- src/components/Reversebuttonplusminus.js
+++ src/components/Reversebuttonplusminus.js
@@ -1,38 +1,34 @@
-import React from "react"
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement } from '../redux/actions/counter'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
-export class Reversebutton extends React.Component {
-  state = {
-    angka: 0
+const Reversebuttonplusminus = () => {
+  const counter = useSelector(state => state.counter)
+  useEffect(() => {
+    console.log('update')
+  })
+  const dispatch = useDispatch()
+  const onInc = () => {
+    dispatch(increment())
   }
-  plus = () => {
-    this.setState({
-      angka: this.state.angka + 1
-    })
-  }
-  minus = () => {
-    if (this.state.angka > 0) {
-      this.setState({
-        angka: this.state.angka - 1
-      })
-    }
+  const onDec = () => {
+    dispatch(decrement())
   }
 
-  render() {
-    return (
-      <div className="day d-flex row justify-content-between align-items-center">
-        <div className="col">
-          <button onClick={this.minus} className="minus"><FaMinus /></button>
-        </div>
-        <div className="col">
-          <div className="count">{this.state.angka}</div>
-        </div>
-        <div className="col">
-          <button onClick={this.plus} className="plus"><FaPlus /></button>
-        </div>
+  return (
+    <div className="day d-flex row justify-content-between align-items-center">
+      <div className="col">
+        <button onClick={onDec} className="minus"><FaMinus /></button>
       </div>
-    )
-  }
+      <div className="col">
+        <div className="count">{counter.num}</div>
+      </div>
+      <div className="col">
+        <button onClick={onInc} className="plus"><FaPlus /></button>
+      </div>
+    </div>
+  )
 }
 
-export default Reversebutton
+export default Reversebuttonplusminus
