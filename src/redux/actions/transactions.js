5da308c07632c275bca8837fd645bc503208df1d
eexxprd <aviantofan@gmail.com>
2022-03-08 15:00:12 +0700

    chore: init transaction redux

---
+++ src/redux/actions/transactions.js
@@ -0,0 +1,15 @@
+import http from '../../helpers/http'
+
+export const postTransaction = (token, data) => {
+  const params = new URLSearchParams()
+  params.append('userId', data.userId)
+  params.append('vehicleId', data.vehicleId)
+  params.append('rentStartDate', data.rentStartDate)
+  params.append('rentEndDate', data.rentEndDate)
+  params.append('prepayment', data.prepayment)
+  params.append('isReturned', data.isReturned)
+  return ({
+    type: 'POST_TRANSACTION',
+    payload: http(token).post('/histories', params)
+  })
+}
