4a289f7279555deeeb23f5e0e93603a2018633a0
eexxprd <aviantofan@gmail.com>
2022-04-25 16:00:11 +0700

    feat: add role conditional rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = () => {
         </div>
       </div>}
 
-      {auth.token !== null && <div className="container d-flex align-items-center">
+      {auth.token !== null && auth.userData.role ==='user' && <div className="container d-flex align-items-center">
         <Link className="navbar-brand text-center" to="/">
           <img className='img-fluid' src={logo} alt="logo" />
           <div className='brand d-inline align-middle'>
@@ -89,6 +89,48 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/history">History</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="">About</Link>
+            </li>
+          </ul>
+          <div className="icon-profile">
+            <FiMail />
+            <Link to="/profile">
+              <img src={auth.userData.image? auth.userData.image:noImage} className="img-rounded-circle" alt="profile" />
+            </Link>
+            <FaPowerOff className='logout' onClick={onLogout} />
+          </div>
+        </div>
+      </div>}
+
+      {auth.token !== null && auth.userData.role ==='admin' && <div className="container d-flex align-items-center">
+        <Link className="navbar-brand text-center" to="/">
+          <img className='img-fluid' src={logo} alt="logo" />
+          <div className='brand d-inline align-middle'>
+            <span className='mx-2'>V E H I C O O L</span>
+          </div>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/addVehicle">Add Vehicle</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/vehicleType">Vehicle</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="">About</Link>
